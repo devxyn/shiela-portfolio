4e5e6a9c952f08fb828e93b0cc1b0fc3f0413653
devxyn <johnlawrencehermo@gmail.com>
2025-05-21 00:36:54 +0800

    feat: Enhance TypeScript and Vite configuration with path aliases and Tailwind CSS integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@components": "/src/components",
+      "@assets": "/src/assets",
+      "@sections": "/src/sections",
+      "@data": "/src/data",
+      "@utils": "/src/utils",
+    },
+  },
+});
