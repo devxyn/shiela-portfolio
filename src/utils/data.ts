5c091ffa462aff77d34dee3c546b7f0e696ac0f7
devxyn <johnlawrencehermo@gmail.com>
2025-05-21 00:38:04 +0800

    feat: Add data module for previous works, contact information, and navigation links

---
+++ src/utils/data.ts
@@ -0,0 +1,54 @@
+export const prevWorks = [
+  {
+    name: "Gala Night Invitation",
+    nameColor: "text-yellow-50",
+    imageUrl: "/images/gala.png",
+    details: [
+      "Work from starch to printing the product.",
+      "One of the hardest to design since it’s my first time designing an invitation, but I enjoyed this project the most.",
+    ],
+  },
+  {
+    name: "Flyers",
+    nameColor: "text-red-50",
+    imageUrl: "/images/flyers.png",
+    details: [
+      "Work from starch to printing the product.",
+      "On this one I focused on the branding of the organization.",
+    ],
+  },
+  {
+    name: "Camp Display Banner",
+    nameColor: "text-yellow-50",
+    imageUrl: "/images/camp.png",
+    details: [],
+  },
+];
+
+export const contactData = [
+  {
+    logo: "/icons/email-icon.svg",
+    label: "shielamaevpalad@gmail.com",
+    link: "mailto:shielamaevpalad@gmail.com",
+  },
+  {
+    logo: "/icons/linkedin-icon.svg",
+    label: "Shiela Mae Palad",
+    link: "https://www.linkedin.com/in/shiela-mae-palad/",
+  },
+];
+
+export const navLinks = [
+  {
+    link: "#about",
+    name: "About",
+  },
+  {
+    link: "#work",
+    name: "Work",
+  },
+  {
+    link: "#previous-work",
+    name: "Previous Work",
+  },
+];
