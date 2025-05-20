c806047e7bda7bdcd03352233191b756090b347d
devxyn <johnlawrencehermo@gmail.com>
2025-05-21 00:38:57 +0800

    feat: Add PreviousWork component to display a list of past projects with a divider

---
+++ src/sections/PreviousWork.tsx
@@ -0,0 +1,18 @@
+import Divider from "@/components/Divider";
+import WorkCard from "@/components/WorkCard";
+import { prevWorks } from "@utils/data";
+
+const PreviousWork = () => {
+  return (
+    <section id='previous-work' className='py-10'>
+      <Divider label='Other projects' />
+      <div className='flex flex-col md:items-center gap-4 lg:gap-10'>
+        {prevWorks.map((item, index) => (
+          <WorkCard item={item} key={index} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default PreviousWork;
